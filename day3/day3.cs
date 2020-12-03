18a6733b7a50da6a5d78ef0e2eec7e32c1ecf47a
DmitryVesh <dmitry.v16@gmail.com>
2020-12-03 07:18:42 +0000

    Pushing day3 challenge completed today
    Started late at 05:25
    
    Part 1: 41 mins
    Part 2: 8 mins
    
    A fun challenge! (made a visual Print of the map being traversed at it is insane how big it gets in some of the sled runs in solution 2)
    
    For Solution 1, had to count the number of a char #, when traversing a map, as if travelled in a diagonal line, e.g. by down row 1, right column 3.
    
    The interesting part of the challenge is that the input given is a repeating unit of the map. So when the sled travels outside the boundry of the input, the input repeats, so I just used try catch, to detect when sled is outside of the map, and then made the sled go back, and appended the repeating unit to the map again.
    
    For solution 2, needed to test other sled runs, with different given gradient (moving down rows and right columns). So just added travelRight & travelDown ints to TraverseMap method, so can work with different gradients. And added list of sleds that held all sled runs.
    The sneaky buggers decided to make the multiplication to get the answer a very large number, which a normal int in c# would be turned negative, so had to spend extra time using a long instead of int.
    
    Solution 1: problems
    Took me a long time to figure out the best way to store and access the map that will be traversed, wrote unneccessay methods to get the char[] from any given map...
    Also spent some time looking up how to catch multiple exceptions at the same time, because either row accessed or column accessed would both fail at different times, so had to look up catching both exceptions, because I didn't want to copy paste same catch exception block, with just a different exception.
    
    Solution 2 problems:
    Hard coded the sleds initially, which was not useful when a negative int was outputed after mutliplication, so wasted time un hard coding using a list, which took additional time.

---
+++ day3/day3.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+public class day3
+{
+    static List<string> ActualMap;
+    static List<string> RepeatingMap;
+
+    public static void Run()
+    {
+        RepeatingMap = GetRepeatingMap();
+
+        //Solution1
+        //int numHitTrees = TraverseMap();
+        //Console.WriteLine($"Hit: {numHitTrees} trees");
+
+        //Solution2
+        List<int> sleds = new List<int>();
+        sleds.Add(TraverseMap(1, 1));
+        sleds.Add(TraverseMap(3, 1));
+        sleds.Add(TraverseMap(5, 1));
+        sleds.Add(TraverseMap(7, 1));
+        sleds.Add(TraverseMap(1, 2));
+
+        long answer = 1;
+        foreach (int sled in sleds)
+        {
+            //Console.WriteLine(sled);
+            answer = answer * sled;
+        }
+        Console.WriteLine($"Multiplied Hit trees of all sleds: {answer}");
+    }
+
+    private static int TraverseMap(int travelRight, int travelDown)
+    {
+        ActualMap = CopyMap(RepeatingMap);
+
+        int treeHitCount = 0;
+        bool reachedBottom = false;
+
+        int currentRow = 0;
+        int currentColumn = 0;
+
+        int bottowRow = ActualMap.Count - 1;
+
+        while (!reachedBottom)
+        {
+            try
+            {
+                //Console.WriteLine($"At currentRow: {currentRow}");
+                currentRow += travelDown;
+                currentColumn += travelRight;
+
+                char[] squaresInCurrentRow = ActualMap[currentRow].ToCharArray();
+                if (squaresInCurrentRow[currentColumn] == '#')
+                    treeHitCount += 1;
+
+                if (currentRow == bottowRow)
+                    reachedBottom = true;
+            }
+            catch (Exception exception)
+            {
+                if (!(exception is IndexOutOfRangeException || exception is ArgumentOutOfRangeException))
+                    throw exception;
+
+                currentRow -= travelDown;
+                currentColumn -= travelRight;
+
+                AddRepeatingMapToActualMap();
+                
+            }
+        }
+        return treeHitCount;
+    }
+    private static void AddRepeatingMapToActualMap()
+    {
+        for (int count = 0; count < RepeatingMap.Count; count++)
+        {
+            ActualMap[count] += RepeatingMap[count];
+        }
+        //PrintMap();
+    }
+    private static void PrintMap()
+    {
+        Console.WriteLine("\n\n\n\n\n\n\n\n");
+        foreach(string row in ActualMap)
+        {
+            Console.WriteLine(row);
+        }
+    }
+    private static List<string> CopyMap(List<string> map)
+    {
+        return map.GetRange(0, map.Count);
+    }
+
+    private static List<string> GetRepeatingMap()
+    {
+        List<string> entries = new List<string>();
+        StreamReader sr = new StreamReader("D:/C# projects/AdventOfCode2020/day3/day3.txt");
+
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            if (line != null)
+                entries.Add(line);
+            else
+                break;
+        }
+        return entries;
+    }
+}
