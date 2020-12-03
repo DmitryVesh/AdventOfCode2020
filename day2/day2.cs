5819bf1e215ef1d0ce04aaf1a45003c03c07e9b5
DmitryVesh <dmitry.v16@gmail.com>
2020-12-03 06:56:24 +0000

    Pushing previously solved day2 challenge.
    Started challenge on time at 05:00.
    
    Checking if a string (password) contains a boundry range of a given character, for solution 1.
    Took time to figure out how to quickly count the number of chars in a string, no simple method built in for that, so had to use Linq, which not familiar in... (probably would have spent less time if just made a method that goes through all chars in string and increases char count if reads a certain char... oh well)
    
    Solution 2 was easier, due to need to have a char XOR at 2 given positions.
    
    Took longer than wanted because I didn't know quick way to iterate through the string to get char count of a specific char. Also during solution 2, forgot the indexes given in input start at 1, so had to -1 the index, but instead I thought it was part of the challenge so implemented a whole try catch block for it... What a donkey.
    
    Part 1: 30 mins
    Part 2: 9 mins

---
+++ day2/day2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class day2
+{
+    public static void Run()
+    {
+        List<string> entries = new List<string>();
+
+        StreamReader sr = new StreamReader("D:/C# projects/AdventOfCode2020/day2/day2.txt");
+
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            if (line != null)
+                entries.Add(line);
+            else
+                break;
+        }
+
+        int validCount = 0;
+        foreach (string entrie in entries)
+        {
+            //Console.WriteLine();
+            string[] splitOnce = entrie.Split(" ");
+
+            string[] minMax = splitOnce[0].Split("-");
+            int min = int.Parse(minMax[0]);
+            int max = int.Parse(minMax[1]);
+            //Console.WriteLine($"{min} {max}");
+            string letter = splitOnce[1].Substring(0, 1);
+            //Console.WriteLine(letter);
+            string password = splitOnce[2];
+            //Console.WriteLine(password);
+            int count = 0;
+            count = password.Count(x => x == char.Parse(letter));
+
+            //Console.WriteLine(count);
+
+            //Solution1
+            //if (min <= count && count <= max)
+            //validCount++;
+
+            //Solution2
+            if (password[min - 1].ToString() == letter && !(password[max - 1].ToString() == letter))
+            {
+                validCount++;
+            }
+
+
+            if (!(password[min - 1].ToString() == letter) && (password[max - 1].ToString() == letter))
+            {
+                validCount++;
+            }
+
+        }
+        Console.WriteLine(validCount);
+
+    }
+}
