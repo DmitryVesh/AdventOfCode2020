94b042a1e0716ab33efd61922a51681c0d97a379
DmitryVesh <dmitry.v16@gmail.com>
2020-12-03 07:28:04 +0000

    Pushing ReadInputFile class.
    Created a class which will handle reading the input file of each challenge starting from day4.
    Returning a List of strings, where each string is the line in text file.
    
    Made so each dayX class day4 onward, doesn't need own method to read the input.

---
+++ ReadInputFile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    public static class ReadInputFile
+    {
+        public static List<string> GetInputAsLines(string path)
+        {
+            List<string> entries = new List<string>();
+            StreamReader sr = new StreamReader(path);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (line != null)
+                    entries.Add(line);
+                else
+                    break;
+            }
+            return entries;
+        }
+    }
+}
